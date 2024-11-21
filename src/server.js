acd495d63d5a50c5146105ddf2df4a4eacff2ac5
Felipe <feliperocha.dev27@gmail.com>
2024-11-21 12:25:02 -0300

    salvando users em memoria

--- src/server.js
+++ src/server.js
@@ -1,13 +1,21 @@
 import http from "node:http";
 
+const users = [];
+
 const server = http.createServer((req, res) => {
   const { method, url } = req;
 
   if (method === "GET" && url === "/users") {
-    return res.end("Listagem de usuários");
+    const usersJson = JSON.stringify(users);
+    return res.setHeader("Content-type", "aplication/json").end(usersJson);
   }
 
   if (method === "POST" && url === "/users") {
+    users.push({
+      id: 1,
+      name: "John Doe",
+      email: "johndoe@j.com",
+    });
     return res.end("Criar usuário");
   }
 
