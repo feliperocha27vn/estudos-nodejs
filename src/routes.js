154c1e84f4eb8b44c3fd4bfdd41718587114fc41
Felipe Rocha <feliperocha.dev27@gmail.com>
2024-11-28 17:39:34 -0300

    fazendo as validações para a URL receber o id

--- src/routes.js
+++ src/routes.js
@@ -45,6 +45,8 @@ export const routes = [
   {
     method: "DELETE",
     path: buildRoutePath("/users/:id"),
-    handler(req, res) {},
+    handler: (req, res) => {
+      return res.end();
+    },
   },
 ];
--- src/utils/build-route-path.js
+++ src/utils/build-route-path.js
@@ -1,6 +1,11 @@
 // /users/:id
 export function buildRoutePath(path) {
+  // ajuda indentificar a expressão contida na url
   const routeParametersRegex = /:([a-zA-Z]+)/g;
+  
+  const pathWithParams = path.replace(routeParametersRegex, "([a-z0-9\-_]+)");
 
-  return new RegExp()
+  const pathRegex = new RegExp(`^${pathWithParams}`);
+
+  return pathRegex;
 }
